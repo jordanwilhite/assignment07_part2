858781502ae63b839e2fe7c1cf5b79086abea6ca
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-15 14:57:29 -0500

    added more answers

--- js/exercises.js
+++ js/exercises.js
@@ -3,13 +3,14 @@
 // ---------------------
 
 function max(a, b){
-    if (a > b);{
-      return a;
-    };
-    else {
+    if (a > b){
+
+    }return a;
+
+    else{
       return b;
     };
-};
+}''
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
@@ -32,7 +33,7 @@ function maxOfThree(a, b, c){
 // ---------------------
 
 function isVowel(char){
-  if (char === a || e || i || o || u || y);{
+  if (char === "a" || "e" || "i" || "o" || "u" || "y"){
     return true;
   };
   else {
@@ -45,14 +46,16 @@ function isVowel(char){
 // ---------------------
 
 function rovarspraket(phrase){
-
-
-  function translate(phrase){
-
-  };
+  var newString = "";
+  for(var i = 0;i < phrase.length; i++){
+  if(!isVowe(phrase[i]) && phrase[i] !=== " "){
+  newString + = phrase[i] + "o" + phrase[i];
+  }
+  else {
+    newString += phrase[i];
+  }
+} return newString;
 };
-
-
 
 // ---------------------
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
@@ -72,23 +75,26 @@ function multiply([2,3,4,5]){
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse("knowledge"){
-  return "egdelwonk";
-};
+function reverse(phrase){
+  var str = "";
+  for (var i= phrase.length -1;
+    i >= 0; i--){
+      str += phrase[i];
+    };
+    return;
+  }
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
 
 function findLongestWord([dog, cat, hippo]){
-  if ([0].length > [1].length && [0].length > [2].length);{
+  if ([0].length > [1].length && [0].length > [2].length){
     return [0];
-  };
-    else if ([1].length > [2].length && [1] > [0].length) {
+  } else if ([1].length > [2].length && [1] > [0].length) {
     return[1];
-    };
-    else if ([2].length > [0].length && [2].length > [1].length) {
-    return [2]
+    } else if ([2].length > [0].length && [2].length > [1].length) {
+    return [2];
     };
 };
 
@@ -96,12 +102,12 @@ function findLongestWord([dog, cat, hippo]){
 // Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
 // ---------------------
 
-function filterLongWords([steve, pirate, dodgeball. spaceship, pluto] i){
-    if [steve, pirate, dodgeball. spaceship, pluto] > i;
+function filterLongWords([steve, pirate, dodgeball. spaceship, pluto] [i]){
+    if [steve, pirate, dodgeball. spaceship, pluto] > [i];
     return [steve, pirate, dodgeball. spaceship, pluto];
   };
   else {
-    return i;
+    return [i];
   };
 };
 
@@ -109,6 +115,6 @@ function filterLongWords([steve, pirate, dodgeball. spaceship, pluto] i){
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
 
-function charFreq(string){
-    //...
-}
+function charFreq("abbabcbdbabdbdbabababcbcbab"){
+
+};
